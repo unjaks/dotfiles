5281153b4809598e46a6cb4e19a173130ba36218
unjaks <un.jako@gmail.com>
2024-01-29 03:34:49 +0700

    another cool commit

---
+++ .config/nvim/init.lua
@@ -0,0 +1,35 @@
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+Plug 'ellisonleao/gruvbox.nvim'
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-tree/nvim-web-devicons'
+Plug 'nvim-lualine/lualine.nvim'
+Plug 'nanozuki/tabby.nvim'
+
+vim.call('plug#end')
+
+require('gruvbox').setup{
+	transparent_mode = true,
+}
+require('lualine').setup{
+	options = {
+		theme = 'gruvbox',
+	}
+}
+
+vim.o.background = "dark"
+vim.o.showtabline = 2
+
+vim.cmd([[colorscheme gruvbox]])
+
+vim.cmd('set number')
+vim.cmd('set cursorline')
+
+vim.filetype.add {
+  extension = {
+    luau = "luau",
+  },
+}
+
